5d07cbfd3c26f0646574345d98db76bbf89aa6fa
mikeyroy <github@mikepearce.us>
2014-09-07 11:32:50 -0700

    Added messaging to show on pages where no badge reviews are found

--- modules/aws/formatReviews.js
+++ modules/aws/formatReviews.js
@@ -1,12 +1,7 @@
 module.exports = function(req, res, badgeReviews, timeStart, pages, reviewCount) {
-	if (reviewCount) {
-		res.render('aws/formatReviews', {
-			badgeReviews: badgeReviews,
-			timeStart: timeStart,
-			pages: pages
-		});
-	} else {
-		res.send('NoResults');
-		res.end();
-	}
+	res.render('aws/formatReviews', {
+		badgeReviews: badgeReviews,
+		timeStart: timeStart,
+		pages: pages
+	});
 };
