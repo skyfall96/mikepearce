2f3f9f0bc2e650d607018b8ee646ef6dc772ecd8
mikeyroy <github@mikepearce.us>
2018-06-20 04:43:06 -0700

    Adding webp images as an option for the background

--- modules/backgrounds/getRandomImage.js
+++ modules/backgrounds/getRandomImage.js
@@ -1,5 +1,11 @@
 let fs = require('fs');
 let path = require('path');
+let browser = require('browser-detect');
 let backgrounds = fs.readdirSync('./public/images/backgrounds');
 
-module.exports = () => path.join(__dirname, '../../public/images/backgrounds', backgrounds[Math.round(Math.random() * backgrounds.length)]);
+module.exports = (req, res) => {
+  const isChrome = browser(req.headers['user-agent']).name === 'chrome';
+  const backgroundDirectory = isChrome ? '../../public/images/backgrounds-webp' : '../../public/images/backgrounds';
+  const backgroundExtension = isChrome ? '.webp' : '.jpg';
+  return path.join(__dirname, backgroundDirectory, backgrounds[Math.round(Math.random() * backgrounds.length)].replace('.jpg', backgroundExtension));
+}
